55b7b84776ba6d90604487a64bcfb411698d706a
MaJloe3Jlo <klestoff.d@gmail.com>
2017-06-02 15:18:49 +0300

    Chapter 10 page 4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -127,14 +127,23 @@ describe User do
   describe "microposts associations" do
     before { @user.save }
     let!(:older_micropost) do
-      FactoryGirl.create(:micropost, @user, created_at: 1.day.ago)
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
     end
     let!(:newer_micropost) do
-      FactoryGirl.create(:micropost, @user, created_at: 1.hour.ago)
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
     end
 
     it "should have right microposts in the right order" do
       expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
-    end  
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.to_a
+      @user.destroy
+      expect(microposts).not_to be_empty
+      microposts.each do | micropost |
+        expect(Micropost.where(id: micropost.id)).to be_empty
+      end  
+    end 
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -95,7 +95,7 @@ describe "User pages" do
 
               it { should have_title(user.name) }
               it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-                describe "followed by signout" do #Notice that this describe is inside the other one
+                describe "followed by signout" do 
                   before { click_link "Sign out" }
                   it { should have_link('Sign in') }
                  end
@@ -141,4 +141,21 @@ describe "User pages" do
         specify { expect(user.reload.email).to eq new_email }      
       end  
     end  
+
+    describe "profile page" do
+      let(:user) { FactoryGirl.create(:user) }
+      let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+      let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+      before { visit user_path(user) }
+
+      it { should have_content(user.name) }
+      it { should have_title(user.name) }
+
+      describe "microposts" do
+        it { should have_content(m1.content) }
+        it { should have_content(m2.content) }
+        it { should have_content(user.microposts.count) }
+      end
+    end
 end
